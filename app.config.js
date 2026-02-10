67d4e7f4a75e8e647d8707e8c179cd8c29156d04
bartugenc55 <bartugenc55@gmail.com>
2026-02-11 02:50:36 +0300

    Configure Supabase for release build

---
+++ app.config.js
@@ -0,0 +1,9 @@
+export default ({ config }) => ({
+  ...config,
+  extra: {
+    ...config.extra,
+    supabaseUrl: "https://xffqvpvqzxlgnuqpleyr.supabase.co",
+    supabaseAnonKey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmZnF2cHZxenhsZ251cXBsZXlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNDAwMzksImV4cCI6MjA4NTYxNjAzOX0.pMX30YjohLpwqZ7iav6eq0gIlpX0AuTL9H0yMzyQrkY",
+  },
+});
